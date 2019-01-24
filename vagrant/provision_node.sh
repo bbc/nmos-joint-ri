59bd9c659bbb7ab63f5c27fc10c586288d2744e0
Andrew Bonney <andrew.bonney@bbc.co.uk>
2019-01-24 10:59:59 +0000

    provision: rename reverse proxy package

--- vagrant/provision_node.sh
+++ vagrant/provision_node.sh
@@ -51,7 +51,7 @@ install -m 666 /dev/null /var/log/nmos.log
 cd /home/vagrant/nmos-reverse-proxy
 git checkout $REVERSE_PROXY_BRANCH
 make dsc
-mk-build-deps --install deb_dist/nmos-reverseproxy_*.dsc --tool "$APT_TOOL"
+mk-build-deps --install deb_dist/nmosreverseproxy_*.dsc --tool "$APT_TOOL"
 make deb
 dpkg -i ../ips-reverseproxy-common_*_all.deb
 sudo apt-get -f -y install
