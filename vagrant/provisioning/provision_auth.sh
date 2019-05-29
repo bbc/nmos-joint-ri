840fb768cc945a2711f74460d1fbb6956c741e95
James Gibson <james.gibson@bbc.co.uk>
2019-05-29 14:49:13 +0100

    Install nmoscommon using ansible

--- vagrant/provisioning/provision_auth.sh
+++ vagrant/provisioning/provision_auth.sh
@@ -14,13 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-COMMON_BRANCH=$1
-MDNS_BRIDGE_BRANCH=$2
-REVERSE_PROXY_BRANCH=$3
-AUTH_BRANCH=$6
-
 export DEBIAN_FRONTEND=noninteractive
-APT_TOOL='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y'
 
 # All service are run as an ipstudio user
 useradd ipstudio
@@ -28,16 +22,3 @@ mkdir /home/ipstudio
 chown -R ipstudio /home/ipstudio
 
 sed -i 's/# deb-src/deb-src/' /etc/apt/sources.list
-apt-get update
-apt-get install python-pip python3-pip python-mock python3-mock devscripts debhelper equivs python3-setuptools python-stdeb python3 libffi-dev tox -y
-pip install setuptools
-
-cd /home/vagrant
-
-git clone --verbose https://github.com/bbc/nmos-common.git
-
-cd /home/vagrant/nmos-common
-git checkout $COMMON_BRANCH
-pip install -e .
-pip3 install -e .
-install -m 666 /dev/null /var/log/nmos.log
